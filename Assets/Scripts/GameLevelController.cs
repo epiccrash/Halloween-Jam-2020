df3d11829deaaf1437a7082c894dd7d34c583968
gcwhitfield <gwhitfieldjr@gmail.com>
2020-10-18 01:06:17 -0400

    Create GameLevelController.cs, GameManager.cs

---
+++ Assets/Scripts/GameLevelController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// LevelController is used to manage overall flow of game. Methods defined in
+// LevelController are accessible by referencing LevelController.Instance
+public class UnitySingleton<GameLevelController> : MonoBehaviour
+{
+    bool _paused = false;
+    [Header("Pause")]
+    public KeyCode pauseKey;
+    public GameObject pauseScreen;
+
+
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void BeginPhaseOne()
+    {
+
+    }
+
+    void BeginPhaseTwo()
+    {
+
+    }
+
+    // pause/unpause the game
+    void TogglePause()
+    {
+        // start/stop time
+        Time.timeScale = _paused ? 0 : 1;
+
+        // display pause screen
+
+    }
+
+    // 
+    void Lose()
+    {
+        // display lose screen
+    }
+
+    // executed when the player successfully completes level
+    void Win()
+    {
+       // display win screen
+    }
+
+    
+}
