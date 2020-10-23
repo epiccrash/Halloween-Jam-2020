1cb104d354e785a03581262cfe259fd4349a1a25
Joey Perrino <epiccrashv3@gmail.com>
2020-10-23 02:39:19 -0500

    Improved menu scene - controls added

---
+++ Assets/Scripts/ButtonHover.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class ButtonHover : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerClickHandler
+{
+    private Color normalColor;
+    private Color hoverColor;
+
+    private Image image;
+
+    private void Awake()
+    {
+        image = GetComponent<Image>();
+
+        normalColor = image.color;
+        hoverColor = new Color(0, 0, 0, 1);
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        image.color = hoverColor;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        image.color = normalColor;
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        image.color = normalColor;
+    }
+}
